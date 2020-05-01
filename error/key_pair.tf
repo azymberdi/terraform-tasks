a463adbb9c0f3f2faa5acaffabc79ca2483626df
EC2 Default User <balogunrash@yahoo.com>
2020-05-01 04:26:11 +0000

    tasks

--- error/key_pair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "eu-west-1-key" {
-  key_name   = "${var.key_name}"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
--- error/vpc.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_vpc" "task3" {
-  cidr_block = "${var.vpc_cidr}"
-  tags        = "${var.tags}"
-}
