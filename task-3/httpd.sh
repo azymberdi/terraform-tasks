96f815b963c92962af6b3b30fb20a50fe9ed800a
EC2 Default User <balogunrash@yahoo.com>
2020-05-01 03:58:49 +0000

    task3

--- task-3/httpd.sh
+++ task-3/httpd.sh
@@ -3,4 +3,4 @@ sudo yum install httpd -y
 sudo systemctl enable httpd
 sudo systemctl start httpd
 
-echo "please stay home, stay safe for the frontline workes" > /var/www/html/index.html
+echo "please stay home, stay safe for the frontline workers" > /var/www/html/index.html
