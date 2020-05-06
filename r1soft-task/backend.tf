2661586a24056cbbac7b9e3996528ba3591aa644
azymberdi <azymberdi@gmail.com>
2020-05-06 07:01:07 +0000

    changes

--- r1soft-task/backend.tf
+++ r1soft-task/backend.tf
@@ -2,9 +2,7 @@ terraform {
   required_version = "0.11.14"
 
   backend "s3" {
-    bucket = "rash-terraform-bucket"
-
-    #dynamodb_table    =   "state_class"
+    bucket = "terraform-bucket-azym"
     key    = "path/to/my/key"
     region = "us-east-1"
   }
